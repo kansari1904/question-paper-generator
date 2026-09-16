438115e0f720662038e2ce94beabe28276b95ba8
kansari1904 <kansari1904@gmail.com>
2026-09-16 19:04:03 +0530

    fix: configure production cors

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://question-paper-generator-zeta.vercel.app/",
+        "https://question-paper-generator-zeta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
